35045f7f016bb594ad14d8fecefc2c02c2db07f1
Camilo Beltran <cambelf.dev@gmail.com>
2023-01-20 00:53:38 -0500

    update images route

--- assets/js/data/products.mjs
+++ assets/js/data/products.mjs
@@ -3,7 +3,7 @@ export const products = [
     name: 'Vintage Backbag',
     price: 94.99,
     discount: 54.99,
-    image: '/assets/images/photo1.png',
+    image: './assets/images/photo1.png',
     amount: 1,
     shipping: 11,
     standard_name: 'vintage-backbag'
@@ -12,7 +12,7 @@ export const products = [
     name: 'Levi Shoes',
     price: 124.99,
     discount: 74.99,
-    image: '/assets/images/photo2.png',
+    image: './assets/images/photo2.png',
     amount: 1,
     shipping: 9.5,
     standard_name: 'levi-shoes'
